5580727b5caffa262e2e6ab90e69fd874e4e423b
Asanka Siriwardena <asankabg@gmail.com>
2023-04-01 00:51:50 +0530

    Default message when no bookmarks

--- src/js/view/bookMarksView.js
+++ src/js/view/bookMarksView.js
@@ -2,6 +2,15 @@ import PreviewView from './previewView';
 
 class BookMarkView extends PreviewView {
     _parentElement = document.querySelector('ul.bookmarks__list');
+    #defaultDiv = this._parentElement.querySelector('.message');
+
+    _insertMarkUp(position, markUp) {
+        if(markUp) {
+            super._insertMarkUp(position, markUp);
+        }else {
+            this._parentElement.innerHTML = this.#defaultDiv.outerHTML;
+        }
+    }
 }
 
 export default new BookMarkView();
